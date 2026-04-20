9f9412e666c64ab5c6a654cc7889ab3cb36296f3
Melycode <melissagc21@gmail.com>
2026-04-20 11:29:49 +0200

    add changes

--- src/modelo/Actor.java
+++ src/modelo/Actor.java
@@ -1,4 +1,73 @@
 package modelo;
 
+import java.util.List;
+
 public class Actor {
+    private int id;
+    private String nombre;
+    private String nacionalidad;
+    private int edad;
+    private List<Pelicula> peliculas;
+
+    public Actor(int id, String nombre, String nacionalidad, int edad, List<Pelicula> peliculas) {
+        this.id = id;
+        this.nombre = nombre;
+        this.nacionalidad = nacionalidad;
+        this.edad = edad;
+        this.peliculas = peliculas;
+    }
+
+    public Actor() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public List<Pelicula> getPeliculas() {
+        return peliculas;
+    }
+
+    public void setPeliculas(List<Pelicula> peliculas) {
+        this.peliculas = peliculas;
+    }
+
+    @Override
+    public String toString() {
+        return "Actor{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", nacionalidad='" + nacionalidad + '\'' +
+                ", edad=" + edad +
+                ", peliculas=" + peliculas +
+                '}';
+    }
 }
